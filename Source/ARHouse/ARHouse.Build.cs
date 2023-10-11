adcf12e88d3c359c9de503aac4182cb83b2143f5
BanBanjh <banjh9nature12@gmail.com>
2023-10-11 21:22:45 +0900

    Commit BJH 01
    Default Setting1

--- Source/ARHouse/ARHouse.Build.cs
+++ Source/ARHouse/ARHouse.Build.cs
@@ -8,7 +8,7 @@ public class ARHouse : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AugmentedReality", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
