b623f1d696957481080c3736f7b71b2d70755c15
김채린 <111097591+cjssdfahkhdfudghsofiadsfipodsdfjg@users.noreply.github.com>
2023-10-11 17:32:10 +0900

    2023.10.11

---
+++ Source/ARHouse/ARHouse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ARHouse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ARHouse, "ARHouse" );
---
+++ Source/ARHouse/ARHouseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ARHouseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARHOUSE_API AARHouseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
