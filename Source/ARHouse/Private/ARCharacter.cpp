f94d80dad71b17981bce232b37f281f0b86e3849
김채린 <111097591+cjssdfahkhdfudghsofiadsfipodsdfjg@users.noreply.github.com>
2023-10-18 09:12:09 +0900

    10.18+ARCharacter

--- Source/ARHouse/Private/ARCharacter.cpp
+++ Source/ARHouse/Private/ARCharacter.cpp
@@ -6,9 +6,9 @@
 #include <GameFramework/Pawn.h>
 #include <Camera/CameraComponent.h>
 #include <GameFramework/SpringArmComponent.h>
+#include "ARBlueprintLibrary.h"
 #include <Kismet/GameplayStatics.h>
 #include "MapToolManager.h"
-#include "ARBlueprintLibrary.h"
 
 // Sets default values
 AARCharacter::AARCharacter()
@@ -67,35 +67,22 @@ void AARCharacter::Tick(float DeltaTime)
 		GetWorld()->SpawnActor<AActor>(chair_BP, GetTouchLocation(firstTouch), FRotator::ZeroRotator, param);
 	}
 
-// 	mapToolManager = Cast<AMapToolManager>(UGameplayStatics::GetActorOfClass(GetWorld(), AMapToolManager::StaticClass()));
-// 
-// 	if (mapToolManager->isAClick || mapToolManager->isDClick)
-// 	{
-// 		FVector Ap0 = GetActorForwardVector();
-// 		FVector Avt = Dir * 500 * DeltaTime;
-// 		FVector Ap = Ap0 + Avt;
-// 		SetActorLocation(Ap, true);
-// 	}
-// 
-// 	if (mapToolManager->isWClick)
-// 	{
-// 		AddMovementInput(GetActorForwardVector(), speed);
-// 	}
-// 
-// 	if (mapToolManager->isSClick)
-// 	{
-// 		AddMovementInput(-GetActorForwardVector(), speed);
-// 	}
-// 
-// 	if (mapToolManager->isAClick)
-// 	{
-// 		AddMovementInput(-GetActorForwardVector(), speed);
-// 	}
-// 
-// 	if (mapToolManager->isDClick)
-// 	{
-// 		AddMovementInput(GetActorRightVector(), speed);
-// 	}
+	if (bIsDragging && ClickedActor)
+	{
+		FVector WorldOrigin;
+		FVector WorldDirection;
+
+		// 현재 마우스 위치 월드 좌표로 변환하기
+		UGameplayStatics::GetPlayerController(this, 0)->DeprojectMousePositionToWorld(WorldOrigin, WorldDirection);
+
+		// 새로운 위치 계산하기
+		FVector DragEndPosition = WorldOrigin + (WorldDirection * 1000.f); // 이동 거리 조절 가능
+
+		// 액터의 위치 업데이트하기
+		FVector DragDelta = DragEndPosition - DragStartLocation;
+		DragDelta.Z = 0;
+		ClickedActor->SetActorLocation(DragStartLocation + DragDelta);
+	}
 
 }
 
@@ -109,6 +96,9 @@ void AARCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	PlayerInputComponent->BindAxis(TEXT("Vertical"), this, &AARCharacter::Vertical);
 	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &AARCharacter::Turn);
 	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AARCharacter::LookUp);
+
+	PlayerInputComponent->BindAction("MouseLeftButton", IE_Pressed, this, &AARCharacter::OnLeftMouseButtonPressed);
+	PlayerInputComponent->BindAction("MouseLeftButton", IE_Released, this, &AARCharacter::OnLeftMouseButtonReleased);
 
 	PlayerInputComponent->BindAction("Cast", IE_Pressed, this, &AARCharacter::ray);
 }
@@ -160,7 +150,13 @@ void AARCharacter::ray()
 					HitResult.GetActor()->GetName());
 			}
 		}
+
+		if (GetWorld()->LineTraceSingleByProfile(HitResult, Start, End, FName("Bed")))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("HitResult"));
+		}
 	}
+
 }
 
 void AARCharacter::ShowPlaneOutLine()
@@ -199,4 +195,26 @@ FVector AARCharacter::GetTouchLocation(const FVector2D& touchPos)
 		return spawnLocation;
 	}
 	return FVector::ZeroVector;
+}
+
+
+void AARCharacter::OnLeftMouseButtonPressed()
+{
+	// 클릭된 액터 가져오기 (마우스 커서 아래에 있는)
+	FHitResult HitResult;
+	UGameplayStatics::GetPlayerController(this, 0)->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, HitResult);
+
+	if (HitResult.GetActor() && HitResult.GetActor()->IsA<ABed>())
+	{
+		ClickedActor = HitResult.GetActor();
+		DragStartLocation = ClickedActor->GetActorLocation();
+		bIsDragging = true;
+	}
+}
+
+void AARCharacter::OnLeftMouseButtonReleased()
+{
+	// 드래그 종료
+	bIsDragging = false;
+	UE_LOG(LogTemp, Warning, TEXT("sdqasdqwd"));
 }
